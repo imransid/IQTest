1867e51f4914c560480c7b41cda5a0040f8189b4
Imran Khan Rafa <emailofimran1992@gmail.com>
2025-03-13 10:43:42 +0600

    cors update

--- apps/telegram/src/main.ts
+++ apps/telegram/src/main.ts
@@ -6,12 +6,5 @@ async function bootstrap() {
   await app.listen(4077);
 
   app.enableCors();
-
-  // Enable CORS
-  // app.enableCors({
-  //   origin: '*', // Allow all origins (change to specific origin for security)
-  //   methods: 'GET,POST,PUT,DELETE',
-  //   allowedHeaders: 'Content-Type,Authorization',
-  // });
 }
 bootstrap();
